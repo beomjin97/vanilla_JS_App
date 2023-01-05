db3a547969b28c1d7a35b980f4cb37e1cc937d31
beomjin97 <qjawls646@gmail.com>
2023-01-05 21:40:06 +0900

    8.2

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const log = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
+
 function onGeoError() {
   alert("Can't find you. No weather for you");
 }
